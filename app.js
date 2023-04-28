a31a80cc0730ae88a1c41f311f508cda2dd60ea7
M-Cie <fauvy.pascal@gmail.com>
2023-04-28 09:29:08 +0200

    last modif

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const btnMenu = document.querySelector('.menu-burger');
-const menu = document.querySelector('.liste-nav');
-btnMenu.addEventListener('click', () => {
-    menu.classList.toggle('active')
-})
+// const btnMenu = document.querySelector('.menu-burger');
+// const menu = document.querySelector('.liste-nav');
+// btnMenu.addEventListener('click', () => {
+//     menu.classList.toggle('active')
+// })
+
+const menuParent = document.querySelectorAll('.liste-nav li.item-nav');
+const menuItems = document.querySelectorAll('.line');
+
+menuParent.forEach((parent, index) => {
+  let menuState = false;
+
+  parent.addEventListener('click', () => {
+    if (!menuState) {
+      menuItems[index].classList.add('active');
+      menuState = true;
+    } else {
+      menuItems[index].classList.remove('active');
+      menuState = false;
+    }
+  });
+});
